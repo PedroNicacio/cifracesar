05a7995c475f01256541c634c0214022ce50b315
PedroNicacio <pnicacio68@gmail.com>
2022-12-02 11:56:31 -0300

    primeiro commit

---
+++ assets/js/javascript.js
@@ -0,0 +1,37 @@
+
+  let encryptBtn = document.getElementById('encrypt-btn');
+        let eInput = document.getElementById('encryptedInput-1')
+        let pInput = document.getElementById('plainInput')
+        let inputs = [eInput,pInput]
+        let copyBtn = document.getElementById('copyToClipBoard')
+
+        inputs.forEach( input => {
+            input.oninput = () => {
+                input.value = input.value.toUpperCase()
+            }
+        })
+
+        function encrypt(){
+            let pInput = documet.getElementById('plainInput-1').value
+            let solved = ''
+            let shiftInput = parseInt(document.getElementById('shiftInput').value)
+
+            for(var i = 0; i< pInput.lenght; i++){
+                let ascii_num = pInput[i].charCodeAt()
+                let sum = ascii_num + shiftInput
+                sum >= 65 && sum <=90 ? solved += String.fromCharCode(sum) : sum > 90 ? solved += String.fromCharCode(65 + (sum & 91)) : solved += pInput[i]
+            }
+            eInput.value = solved
+        }
+
+            function copyText() {
+                eInput.select()
+                eInput.setSelectionRange(0,99999)
+                document.execCommand('copy')
+                alert(`Mensagem Copiada: ${eInput.value}`)
+            }
+        copyBtn.addEventListener('click', Copiar)
+        encryptbtn.addEventListener('click', Criptografar);
+
+
+        
